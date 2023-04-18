9cab34f9ba4b9e3346ded8c104b2500faa8d6dae
Romildo Marques <git@romildo.me>
2023-04-18 17:34:40 -0300

    up

--- src/AtyBackend.Infrastructure.Data/Context/AppDbContextFactory.cs
+++ src/AtyBackend.Infrastructure.Data/Context/AppDbContextFactory.cs
@@ -7,11 +7,19 @@ namespace CleanArchTemplate.Infra.Data.Context
 {
     public class AppDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
     {
+        
+        //public AppDbContextFactory()
+        //{
+        //}
         ApplicationDbContext IDesignTimeDbContextFactory<ApplicationDbContext>.CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
 
-            var stringConnection = "Data Source=localhost;User ID=SA;Password=pw2020@mssql;Initial Catalog=pdm-dev;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //var stringConnection = "Data Source=localhost;User ID=SA;Password=pw2020@mssql;Initial Catalog=pdm-dev;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            //var stringConnection = "Data Source=localhost;User ID=SA;Password=mssql1Ipw;Initial Catalog=pdm-dev;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            // aws romildo
+            var stringConnection = "Data Source=3.87.196.133,1433;User ID=SA;Password=pw2020@mssql;Initial Catalog=pdm-dev;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
             optionsBuilder.UseSqlServer(stringConnection);
 
