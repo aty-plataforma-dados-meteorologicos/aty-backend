3c82c4c5aaed08bd1cf84067c9dad627bb9f9507
Romildo Marques <git@romildo.me>
2023-06-08 17:09:22 -0300

    Add Sensor MeasurementType

--- src/AtyBackend.Domain/Entities/Sensor.cs
+++ src/AtyBackend.Domain/Entities/Sensor.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AtyBackend.Domain.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
@@ -13,6 +14,7 @@ namespace AtyBackend.Domain.Entities
         public string MeasurementUnit { get; set; }
         public double Minimum { get; set; }
         public double Maximum { get; set; }
+        public WeatherMeasurementsEnum MeasurementType { get; set; }
 
         /*
          A acurácia é geralmente expressa como uma porcentagem ou como um valor absoluto em relação ao valor real ou de referência.
